33042fcd2d2591b9a039b847c6f50b1f0b6cb0a7
Sergey UP <sergey10goals@gmail.com>
2024-03-01 23:42:14 +0200

    added mobile navigation with HamburgerMenu component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,38 @@
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { brainmaster } from "../assets";
 import { navigation } from "../constants";
 import Button from "./Button";
+import { HamburgerMenu } from "./design/Header";
+import MenuSvg from "../assets/svg/MenuSvg";
 
 const Header = () => {
   const pathname = useLocation();
+  const [openNavigation, setOpenNavigation] = useState(false);
+
+  const toggleNavigation = () => {
+    if (openNavigation) {
+      setOpenNavigation(false);
+    } else {
+      setOpenNavigation(true);
+    }
+  };
+
+  const handleClick = () => {
+    setOpenNavigation(false);
+  };
 
   return (
-    <div className="fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+    <div
+      className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm 
+      ${openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"}`}
+    >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <a className="block w-[12rem] xl:mr-8" href="#hero">
+        <a
+          className="block w-[12rem] xl:mr-8"
+          href="#hero"
+          onClick={handleClick}
+        >
           <img
             src={brainmaster}
             width={196}
@@ -17,7 +40,11 @@ const Header = () => {
             alt="Brainmaster logo"
           />
         </a>
-        <nav className="fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+        <nav
+          className={`${
+            openNavigation ? "flex" : "hidden"
+          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+        >
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
               <a
@@ -35,6 +62,8 @@ const Header = () => {
               </a>
             ))}
           </div>
+
+          <HamburgerMenu />
         </nav>
         <a
           href="#sign-up"
@@ -42,7 +71,16 @@ const Header = () => {
         >
           New account
         </a>
-        <Button href="#sign-in">Sign in</Button>
+        <Button className="hidden lg:flex" href="#sign-in">
+          Sign in
+        </Button>
+        <Button
+          className="ml-auto lg:hidden"
+          px="px-3"
+          onClick={toggleNavigation}
+        >
+          <MenuSvg openNavigation={openNavigation} />
+        </Button>
       </div>
     </div>
   );
