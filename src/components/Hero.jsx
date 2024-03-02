0550af7a5f8e4b4ffbcaf2b1146ce07e491fd6b8
Sergey UP <sergey10goals@gmail.com>
2024-03-02 12:26:48 +0200

    fixed Hero section height: added customPaddings

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,6 +18,7 @@ const Hero = () => {
       className="pt-[12rem] -mt-[5.25rem]"
       crosses
       crossesOffset="lg:translate-y-[5.25rem]"
+      customPaddings
     >
       <div className="container realtive" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
