4698c035b9074b3f4b2829ceaec230d9cafa825c
Sergey UP <sergey10goals@gmail.com>
2024-03-03 00:06:17 +0200

    created Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import { socials } from "../constants";
+import Section from "./Section";
+
+const Footer = () => {
+  return (
+    <Section className="!px-0 !py-10" crosses>
+      <div className="container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col">
+        <div className="flex flex-col gap-5 text-center sm:gap-2 sm:text-left">
+          <p className="caption">Developer: Sergey UP</p>
+          <p className="caption text-n-4 lg:block">
+            © {new Date().getFullYear()}. All rights reserved.
+          </p>
+        </div>
+
+        <ul className="flex gap-5 flex-wrap">
+          {socials.map((item) => (
+            <a
+              className="flex items-center justify-center w-10 h-10 bg-n-7 rounded-full transition-colors hover:bg-n-6"
+              key={item.id}
+              href={item.url}
+              target="_blank"
+            >
+              <img src={item.iconUrl} width={20} height={20} alt={item.title} />
+            </a>
+          ))}
+        </ul>
+      </div>
+    </Section>
+  );
+};
+
+export default Footer;
