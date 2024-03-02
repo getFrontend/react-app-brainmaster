544c3f89c8e18ac081e04975e8e2fb38982c160f
Sergey UP <sergey10goals@gmail.com>
2024-03-02 13:42:34 +0200

    created Heading component

---
+++ src/components/Heading.jsx
@@ -0,0 +1,16 @@
+const Heading = ({ className, title, accent }) => {
+  return (
+    <div
+      className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20 md:text-center`}
+    >
+      {title && (
+        <h2 className="h2">
+          {title}
+          <span className="text-purple-500">{accent || ""}</span>
+        </h2>
+      )}
+    </div>
+  );
+};
+
+export default Heading;
