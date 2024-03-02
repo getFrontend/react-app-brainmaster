e6bf9b3a08073860ffd9b634a432b8fc0ab3c691
Sergey UP <sergey10goals@gmail.com>
2024-03-02 20:10:01 +0200

    added spin animation for Generating

--- src/components/Generating.jsx
+++ src/components/Generating.jsx
@@ -6,7 +6,7 @@ const Generating = ({ className }) => {
       className={`flex items-center h-[3.5rem] px-6 bg-n-8/80 rounded-[1.7rem] text-base 
       ${className || ""}`}
     >
-      <img className="w-5 h-5 mr-4" src={loading} alt="Loading" />
+      <img className="animate-spin w-5 h-5 mr-4" src={loading} alt="Loading" />
       AI is generating
     </div>
   );
