6c6b3f030efa638e0cff1f5e72736ff05253fa5b
Sergey UP <sergey10goals@gmail.com>
2024-03-02 12:17:19 +0200

    Fully implemented Hero section: added CompanyLogos, Generating and Notification

---
+++ src/components/Generating.jsx
@@ -0,0 +1,15 @@
+import { loading } from "../assets";
+
+const Generating = ({ className }) => {
+  return (
+    <div
+      className={`flex items-center h-[3.5rem] px-6 bg-n-8/80 rounded-[1.7rem] text-base 
+      ${className || ""}`}
+    >
+      <img className="w-5 h-5 mr-4" src={loading} alt="Loading" />
+      AI is generating
+    </div>
+  );
+};
+
+export default Generating;
