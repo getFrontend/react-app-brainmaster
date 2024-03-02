d40d167af291a15cd66ae1097a36f27873a83a9f
Sergey UP <sergey10goals@gmail.com>
2024-03-03 00:18:27 +0200

    changed the order of menu items in the navigation

--- src/constants/index.js
+++ src/constants/index.js
@@ -44,13 +44,13 @@ export const navigation = [
   },
   {
     id: "1",
-    title: "Pricing",
-    url: "#pricing",
+    title: "How to use",
+    url: "#how-to-use",
   },
   {
     id: "2",
-    title: "How to use",
-    url: "#how-to-use",
+    title: "Pricing",
+    url: "#pricing",
   },
   {
     id: "3",
