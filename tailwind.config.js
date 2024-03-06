1f849d2ebdc5bed71ba2d2f2e7463d26d5f88eeb
Sergey UP <sergey10goals@gmail.com>
2024-03-06 20:39:27 +0200

    update: added nice animation  meteors effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,17 @@ export default {
     extend: {
       animation: {
         "spin-slow": "spin 10s linear infinite",
+        "meteor-effect": "meteor 5s linear infinite",
+      },
+      keyframes: {
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+          "70%": { opacity: "1" },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: "0",
+          },
+        },
       },
       colors: {
         color: {
